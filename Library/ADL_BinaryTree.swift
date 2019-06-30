1a99f8feee0ad78d3e87c1b01be3eae2346b1e4b
Adland Lee <adland.lee@gmail.com>
2019-06-30 09:31:21 -0700

    separate BinaryTree and BinarySearchTree

--- Library/ADL_BinaryTree.swift
+++ Library/ADL_BinaryTree.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class ADL_BinaryTree<Element> where Element: Hashable & Comparable {
+class ADL_BinaryTree<Element> {
 
     var value: Element
     var left: ADL_BinaryTree?
@@ -13,27 +13,11 @@ class ADL_BinaryTree<Element> where Element: Hashable & Comparable {
     public var count: Int {
         return (left?.count ?? 0) + 1 + (right?.count ?? 0)
     }
-    
-    public var minValue: Element {
-        var node: ADL_BinaryTree! = self
-        
-        while node.left != nil {
-            node = node.left
-        }
-        
-        return node.value
+
+    public var isEmpty: Bool {
+        return count == 0
     }
-    
-    public var maxValue: Element {
-        var node: ADL_BinaryTree! = self
-        
-        while node.right != nil {
-            node = node.right
-        }
-        
-        return node.value
-    }
-    
+
     public func traverseBreadthFirst(_ process: (Element) -> Void) {
         let pending = ADL_Queue_Array<ADL_BinaryTree<Element>>()
         pending.enqueue(self)
@@ -71,92 +55,5 @@ class ADL_BinaryTree<Element> where Element: Hashable & Comparable {
         right?.traverseOutOrder(process)
         process(value)
         left?.traverseOutOrder(process)
-    }
-}
-
-class ADL_BinarySearchTree<Element> where Element: Hashable & Comparable {
-    
-    var root: ADL_BinaryTree<Element>?
-    
-    var count: Int {
-        return root?.count ?? 0
-    }
-
-    public var isEmpty: Bool {
-        return count == 0
-    }
-
-    func insert(_ element: Element) {
-
-    }
-
-    func removeLeft() -> ADL_BinarySearchTree_Graph?{
-        guard let result = self.left as? ADL_BinarySearchTree_Graph else {
-            return nil
-        }
-
-        let left = self.left as? ADL_BinarySearchTree_Graph
-        if right != nil {
-            self.left = left?.popRightMost()
-        }
-        else {
-            self.left = left?.left
-        }
-
-        result.left = nil
-        result.right = nil
-
-        return result
-    }
-
-    func removeRight() -> ADL_BinarySearchTree_Graph?{
-        guard let result = self.right as? ADL_BinarySearchTree_Graph else {
-            return nil
-        }
-
-        let right = self.right as? ADL_BinarySearchTree_Graph
-        if left != nil {
-            self.right = right?.popLeftMost()
-        }
-        else {
-            self.right = right?.right
-        }
-
-        result.left = nil
-        result.right = nil
-
-        return result
-    }
-
-    func popLeftMost() -> ADL_BinarySearchTree_Graph? {
-        var prev: ADL_BinarySearchTree_Graph!
-        var node: ADL_BinarySearchTree_Graph? = self
-
-        while node?.left != nil {
-            prev = node
-            node = node?.left as? ADL_BinarySearchTree_Graph
-        }
-
-        if let prev = prev {
-            prev.left = nil
-        }
-
-        return node
-    }
-
-    func popRightMost() -> ADL_BinarySearchTree_Graph? {
-        var prev: ADL_BinarySearchTree_Graph!
-        var node: ADL_BinarySearchTree_Graph? = self
-
-        while node?.right != nil {
-            prev = node
-            node = node?.right as? ADL_BinarySearchTree_Graph
-        }
-
-        if let prev = prev {
-            prev.right = nil
-        }
-
-        return node
     }
 }
